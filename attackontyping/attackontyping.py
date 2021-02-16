0ee5c82746a65db8509d5ca6c4184d5bff8ea346
PyDrummer <anthonybeavermusic@gmail.com>
2021-02-15 20:55:48 -0800

    day one game logic

--- attackontyping/attackontyping.py
+++ attackontyping/attackontyping.py
@@ -1,4 +1,7 @@
 import time
+import random
+
+sentences = ['sentence one', 'sentence two', 'sentence three']
 
 ## Time keeping logic
 class TimerError(Exception):
@@ -44,6 +47,7 @@ class Game:
                 print('Selected rules')
             if user_input == 'p':
                 print('Selected play')
+                self.play_game()
             if user_input == 'd':
                 print('Selected devs')
             if user_input == 'e':
@@ -69,24 +73,75 @@ class Game:
 
 
 
+    # Calcuation logic:
+    def calc_score(self, user_time, time_allowed, points):
+        if user_time < time_allowed:
+            self.points += points
+            print(f'Great work! You have {self.points} points! \n')
+            print('Get ready for the next question!\n')
+            time.sleep(2)
+        else:
+            print('')
+            print('Try again')
+            self.lives -= 1
+            print(f'Lives left: {self.lives}!')
+            time.sleep(2)
 
 
 
-
-
+    # Game logic starts here:
     def play_game(self):
-        # easy - extreme rounds in here   
-        pass
+        # easy - extreme rounds in here
+        # while self.lives > 0:
+        while self.points < 15 and self.lives > 0:
+            user_time = self.start_easy_round()
+            # Easy mode: time allowed is 10, points per round 5
+            self.calc_score(user_time, 10, 5)
+            
+        while self.points < 45 and self.lives > 0:
+            self.difficulty = 'Medium'
+            # Med round: time allowed is 10, points per round 10
+            # self.start_med_round()
 
+        while self.points < 100 and self.lives > 0:
+            self.difficulty = 'Hard'
+            # self.start_hard_round()
 
+        while self.points < 200 and self.lives > 0:
+            self.difficulty = 'Extreme'
+            # self.start_extreme_round()
 
-
-
-
-
+        print(f'Game Over! You gained {self.points}. \n Would you like to try again?')
+        user_input = input('(Y)es or (N)o \n> ').lower()
+        if user_input == 'y':
+            self.points = 0
+            self.lives = 3
+            self.play_game()
+        if user_input == 'n':
+            exit()
+        # pass
 
     def start_easy_round(self):
-        pass
+        timekeeper = Timer()
+
+        #Logan's words will replace this:
+        question = random.choice(sentences)
+
+        print("Type the following sentence:")
+        print("-------------------------")
+        print(f'{question}')
+        print("-------------------------")
+        time.sleep(3)
+        print('Ready..?')
+        time.sleep(1)
+        timekeeper.start()
+        answer = input('Start!\n> ')
+        time_stop = timekeeper.stop()
+        print(f"Elapsed time: {time_stop:0.4f} seconds, you typed: {answer}")
+        if answer != question:
+            time_stop = 100
+            return time_stop
+        return time_stop
 
 
 
@@ -119,7 +174,7 @@ class Game:
         pass
 
 
-
+    
 
 
 
--- tests/test_attackontyping.py
+++ tests/test_attackontyping.py
@@ -1,5 +1,16 @@
 from attackontyping import __version__
-
+from attackontyping.attackontyping import Timer
+# import pytest
+import time
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_timer():
+    timer = Timer()
+    timer.start()
+    time.sleep(1)
+    stopper = timer.stop()
+    actual = int(stopper)
+    expected = 1
+    assert actual == expected
