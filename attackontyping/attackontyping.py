42308b8cb88547e2b3ae316df369fffc14338474
Logan Jones <loganemilyjones@mac.com>
2021-02-17 08:46:02 -0800

    move timer class, separate menu functions, minor cleanup

--- attackontyping/attackontyping.py
+++ attackontyping/attackontyping.py
@@ -1,63 +1,44 @@
 import time
 import random
-
 from random_text import RandomText
+from timer import Timer
 
-## Time keeping logic
-class TimerError(Exception):
-    """A custom exception used to report errors in use of Timer class"""
-
-class Timer:
-    def __init__(self):
-      self._start_time = None
-
-    def start(self):
-        """Start a new timer"""
-        if self._start_time is not None:
-            raise TimerError("Timer is running. Use .stop() to stop it")
-
-        self._start_time = time.perf_counter()
-
-    def stop(self):
-        """Stop the timer, and report the elapsed time"""
-        if self._start_time is None:
-            raise TimerError("Timer is not running. Use .start() to start it")
-
-        elapsed_time = time.perf_counter() - self._start_time
-        self._start_time = None
-        return elapsed_time
 
 class Game:
     def __init__(self):
         self.lives = 3
         self.points = 0
-        self.difficulty = 'easy'
 
-    def menu_selection(self):
+    @staticmethod
+    def print_welcome():
+        print('\n\n')
+        print("Welcome Placeholder! \n\n")
         # Print Welcome ASCII
 
-        user_input = None
+    @staticmethod
+    def thanks_for_playing():
+        print('\nThanks for playing!')
+        exit()
 
-        while user_input not in ('r', 'p', 'd', 'e'):
-            print('Would you like to (p)lay the game, read the (r)ules, learn about the (d)evs, or (e)xit?')
-            
-            user_input = input('> ').lower()
-            
-            if user_input == 'r':
-                print('Selected rules')
-                self.rules()
-            if user_input == 'p':
-                print('Selected play')
-                self.play_game()
-            if user_input == 'd':
-                print('Selected devs')
-                self.about_us()
-            if user_input == 'e':
-                print('Thanks for playing!')
-                exit()
-            # if user_input == 'ludacris':
-            #     print('You\'re CRAZY!')
-            #     self.start_ludacris_round()
+    def start_app(self):
+        self.print_welcome()
+        self.menu_selection()
+
+    def menu_selection(self):
+        play = 'Would you like to (p)lay the game, read the (r)ules, learn about the (d)evs, or (q)uit?'
+        user_input = input(play + '\n'+ '> ').lower()
+        
+        if user_input == 'r':
+            self.rules()
+        if user_input == 'p':
+            self.play_game()
+        if user_input == 'd':
+            self.about_us()
+        if user_input == 'q':
+            self.thanks_for_playing()
+        # if user_input == 'ludacris':
+        #     print('You\'re CRAZY!')
+        #     self.start_ludacris_round()
 
     def rules(self):
         # color selector:
@@ -73,63 +54,6 @@ class Game:
 
         """)
         self.menu_selection()
-
-
-    def about_us(self):
-        # about_us(self= None)
-        # green = '\033[32;1;40m'
-        # txt = 'About Us'
-        # center_txt = txt.center(110)
-        # print(green + center_txt )
-        option = None
-
-        # while option != 0:
-        print("[1] Anthony Beaver")
-        print("[2] Nick Dorkins")
-        print("[3] Logan Jones")
-        print("[4] Nebiuy Kifle")
-        print("[0] Go to game page")
-        option = int(input("enter index number: "))
-        # green = '\033[32;1;40m'
-        # print(green + 'about us')
-        if option == 1:
-            print("""
-#################################################################################################################################################################################
-        Anthony Beaver:
-My about me: "Anthony Beaver is a full stack software developer from Seattle Washington.Passionate about problem solving and playing the drums! Looking to change the world one syntax error at a time..
-#################################################################################################################################################################################
-            """)
-            self.about_us()
-        elif option == 2:
-            print("""
-#################################################################################################################################################################################
-            Nick Dorkins:
-Software Developer with a background in Specialized Industrial Safety, and fabrication. Enjoy's traveling and spending time with family and puppies. Favorite things to do outside quarantine are going to Disney parks and playing Magic the Gathering.
-#################################################################################################################################################################################
-            """)
-            self.about_us()
-        elif option == 3:
-            print("""
-#################################################################################################################################################################################
-            Logan Jones:
-Logan Jones is a former product manager and current software engineer. She is a skilled problem solver who loves cats, taco bell, and world of warcraft..
-#################################################################################################################################################################################
-            """)
-            self.about_us()
-        elif option == 4:
-            print("""
-#################################################################################################################################################################################
-            Nebiuy Kifle:
-Hello My name is Nebiyu kifle. I am a professional and highly motivated software engineer.I have a background on marketing and logistics management for about 6 years back home in Ethiopia. I used to also have my own business.  I have worked on multiple projects and have a broad experience in software development at code fellows. I am passionate about to learn new think in techs industry. I would love the opportunity to put my experience at tech company. I am currently looking for a new role that will utilize my skills and experience and take my career to the next level.
-#################################################################################################################################################################################
-            """)
-            self.about_us()
-        elif option == 0:
-            self.menu_selection()
-        else:
-            print("\nPlease enter a valid option\n")
-            self.about_us()
-
 
 # GAME LOGIC STUFF #
 
@@ -148,30 +72,23 @@ Hello My name is Nebiyu kifle. I am a professional and highly motivated software
             time.sleep(2)
 
 
-
-    # Game logic starts here:
     def play_game(self):
-        # easy - extreme rounds in here
-        # while self.lives > 0:
         while self.points < 20 and self.lives > 0:
             user_time = self.start_easy_round()
             # Easy mode: time allowed is 10, points per round 5
             self.calc_score(user_time, 15, 10)
             
         while self.points < 40 and self.lives > 0:
-            self.difficulty = 'Medium'
             # Med round: time allowed is 10, points per round 10
             user_time = self.start_med_round()
             self.calc_score(user_time, 10, 10)
 
         while self.points < 60 and self.lives > 0:
-            self.difficulty = 'Hard'
             user_time = self.start_hard_round()
             self.calc_score(user_time, 20, 10)
             # self.start_hard_round()
 
         while self.lives > 0:
-            self.difficulty = 'Extreme'
             user_time = self.start_extreme_round()
             self.calc_score(user_time, 25, 10)
             # self.start_extreme_round()
@@ -184,6 +101,7 @@ Hello My name is Nebiyu kifle. I am a professional and highly motivated software
             self.play_game()
         if user_input == 'n':
             exit()
+
 
     def start_easy_round(self):
         timekeeper = Timer()
@@ -230,6 +148,7 @@ Hello My name is Nebiyu kifle. I am a professional and highly motivated software
             return time_stop
         return time_stop
 
+
     def start_hard_round(self):
         timekeeper = Timer()
         text = RandomText()
@@ -256,7 +175,7 @@ Hello My name is Nebiyu kifle. I am a professional and highly motivated software
             time_stop = 100
             return time_stop
         return time_stop
-        
+
 
     def start_extreme_round(self):
         timekeeper = Timer()
@@ -325,6 +244,60 @@ Hello My name is Nebiyu kifle. I am a professional and highly motivated software
     
 
 
+    def about_us(self):
+        # about_us(self= None)
+        # green = '\033[32;1;40m'
+        # txt = 'About Us'
+        # center_txt = txt.center(110)
+        # print(green + center_txt )
+        option = None
+
+        # while option != 0:
+        print("[1] Anthony Beaver")
+        print("[2] Nick Dorkins")
+        print("[3] Logan Jones")
+        print("[4] Nebiuy Kifle")
+        print("[0] Go to game page")
+        option = int(input("enter index number: "))
+        # green = '\033[32;1;40m'
+        # print(green + 'about us')
+        if option == 1:
+            print("""
+#################################################################################################################################################################################
+        Anthony Beaver:
+My about me: "Anthony Beaver is a full stack software developer from Seattle Washington.Passionate about problem solving and playing the drums! Looking to change the world one syntax error at a time..
+#################################################################################################################################################################################
+            """)
+            self.about_us()
+        elif option == 2:
+            print("""
+#################################################################################################################################################################################
+            Nick Dorkins:
+Software Developer with a background in Specialized Industrial Safety, and fabrication. Enjoy's traveling and spending time with family and puppies. Favorite things to do outside quarantine are going to Disney parks and playing Magic the Gathering.
+#################################################################################################################################################################################
+            """)
+            self.about_us()
+        elif option == 3:
+            print("""
+#################################################################################################################################################################################
+            Logan Jones:
+Logan Jones is a former product manager and current software engineer. She is a skilled problem solver who loves cats, taco bell, and world of warcraft..
+#################################################################################################################################################################################
+            """)
+            self.about_us()
+        elif option == 4:
+            print("""
+#################################################################################################################################################################################
+            Nebiuy Kifle:
+Hello My name is Nebiyu kifle. I am a professional and highly motivated software engineer.I have a background on marketing and logistics management for about 6 years back home in Ethiopia. I used to also have my own business.  I have worked on multiple projects and have a broad experience in software development at code fellows. I am passionate about to learn new think in techs industry. I would love the opportunity to put my experience at tech company. I am currently looking for a new role that will utilize my skills and experience and take my career to the next level.
+#################################################################################################################################################################################
+            """)
+            self.about_us()
+        elif option == 0:
+            self.menu_selection()
+        else:
+            print("\nPlease enter a valid option\n")
+            self.about_us()
 
 
 
@@ -354,4 +327,4 @@ Hello My name is Nebiyu kifle. I am a professional and highly motivated software
 
 if __name__ == '__main__':
     game = Game()
-    game.menu_selection()
+    game.start_app()
