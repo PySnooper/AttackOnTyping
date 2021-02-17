e8dc1862c32f80d841950f826ea4209e9ccc2a55
Logan Jones <loganemilyjones@mac.com>
2021-02-16 17:36:05 -0800

    refactored RandomText + tests

--- attackontyping/random_text.py
+++ attackontyping/random_text.py
@@ -1,37 +1,19 @@
-import string
 from faker import Faker
 
 class RandomText:
     def __init__(self):
-        self.easy_word_count = 5
-        self.medium_word_count = 10
-        self.hard_word_count = 10
-        self.extreme_word_count = 10
-        self.hard_sent_count = 2
-        self.extreme_sent_count = 5
-        self.fake = Faker().unique
-        pass
+        self.fake = Faker()
 
-    def get_easy_text(self):
-        text = self.fake.sentence(nb_words=self.easy_word_count,variable_nb_words=False)
-        text = text.translate(str.maketrans('', '', string.punctuation)).lower()
-        return text
 
-    def get_medium_text(self):
-        text = self.fake.sentence(nb_words=self.medium_word_count,variable_nb_words=False)
-        text = text.translate(str.maketrans('', '', string.punctuation)).lower()
-        return text
+    def get_words(self, word_count):
+        word_list = self.fake.words(nb=word_count, unique=True)
+        words = ' '.join(word_list)
+        return words
 
-    def get_hard_text(self):
+
+    def get_sentences(self, sentence_count, word_count):
         sentences = []
-        for _ in range(self.hard_sent_count):
-            sentences.append(self.fake.sentence(nb_words=self.hard_word_count,variable_nb_words=False))
-        text = ' '.join(sentences)
-        return text
-
-    def get_extreme_text(self):
-        sentences = []
-        for _ in range(self.extreme_sent_count):
-            sentences.append(self.fake.sentence(nb_words=self.extreme_word_count,variable_nb_words=False))
+        for _ in range(sentence_count):
+            sentences.append(self.fake.sentence(nb_words=word_count, variable_nb_words=False))
         text = ' '.join(sentences)
         return text
--- tests/test_random_text.py
+++ tests/test_random_text.py
@@ -1,36 +1,21 @@
-import string
 from attackontyping.random_text import RandomText
 
 
-def test_easy_text():
+def test_get_words():
     text = RandomText()
-    actual = len(text.get_easy_text().split())
-    expected = text.easy_word_count
-    assert actual == expected
+    word_count = 5
+    actual = len(text.get_words(word_count).split(' '))
+    expected = word_count
+    assert actual == word_count
 
 
-def test_medium_text():
+def test_get_sentences():
     text = RandomText()
-    actual = len(text.get_medium_text().split())
-    expected = text.medium_word_count
-    assert actual == expected
-
-
-def test_hard_text():
-    text = RandomText()
-    actual_sentences = text.get_hard_text().split('.')
-    actual_sentences.remove('')
-    expected_sentence_length = text.hard_sent_count
-    right_length = all(len(sent.split()) == text.hard_word_count for sent in actual_sentences)
-    assert len(actual_sentences) == expected_sentence_length
-    assert right_length
-
-
-def test_extreme_text():
-    text = RandomText()
-    actual_sentences = text.get_extreme_text().split('.')
-    actual_sentences.remove('')
-    expected_sentence_length = text.extreme_sent_count
-    right_length = all(len(sent.split()) == text.extreme_word_count for sent in actual_sentences)
-    assert len(actual_sentences) == expected_sentence_length
-    assert right_length
+    sentence_count = 2
+    word_count = 5
+    sentence_list = text.get_sentences(sentence_count, word_count).split('. ')
+    actual_sentence_count = len(sentence_list)
+    words_per_sentence = [len(sentence.split()) for sentence in sentence_list]
+    words_match_count = all(count == word_count for count in words_per_sentence)
+    assert words_match_count
+    assert actual_sentence_count == sentence_count
