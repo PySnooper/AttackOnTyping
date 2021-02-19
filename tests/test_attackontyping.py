7b8bebf6e55c1cde6450e2c40386f13654d2fcdb
Logan Jones <loganemilyjones@mac.com>
2021-02-19 12:55:18 -0800

    100% coverage on /timer and /random_text

--- tests/test_attackontyping.py
+++ tests/test_attackontyping.py
@@ -1,19 +1,8 @@
 from attackontyping import __version__
-from attackontyping.timer import Timer
 # import pytest
-import time
 
 def test_version():
     assert __version__ == '0.1.0'
-
-def test_timer():
-    timer = Timer()
-    timer.start()
-    time.sleep(1)
-    stopper = timer.stop()
-    actual = int(stopper)
-    expected = 1
-    assert actual == expected
 
 #  TODO  Test game methods
 #  TODO  Test Mode method
