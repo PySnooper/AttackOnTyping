81dc38220a3724aa346ade81036e9b71f1fc8b9d
Logan Jones <loganemilyjones@mac.com>
2021-02-18 19:04:57 -0800

    about us method for logan

--- attackontyping/about.py
+++ attackontyping/about.py
@@ -2,13 +2,9 @@ from colorama import Fore, Back, Style
 import pyfiglet
 
 def logan():
-    print('\n\n')
-    print("****************************")
+    print("*******************************")
     print(Fore.GREEN)
-    print(pyfiglet.figlet_format("Logan", 'banner'))
+    print(pyfiglet.figlet_format("Logan"))
     print(Style.RESET_ALL)
-    print("****************************")
-    print()
-    print("Other stuff about me whatever\nOther stuff about me whatever\nOther stuff about me whatever")
-
-logan()
+    print("*******************************")
+    print("\nLogan Jones is a former product manager and current\nsoftware engineer. She is a skilled problem solver\nwho loves cats, taco bell, and world of warcraft.")
