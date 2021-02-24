ea949a02d4028cd62899ef84ff5580ee2f688ac4
Logan Jones <loganemilyjones@mac.com>
2021-02-23 17:59:24 -0800

    about us tests, menu test, run app test

--- tests/test_attackontyping.py
+++ tests/test_attackontyping.py
@@ -1,12 +1,12 @@
-from attackontyping.attackontyping import run_app, menu, game_over, about_us, print_rules, print_lives, print_points
+from attackontyping.attackontyping import run_app, menu, game_over, print_rules, print_lives, print_points, print_about_menu, about_us
 from gamelogic.gamelogic import GameLogic
-from ascii_art.ascii import game_over_ascii
+from ascii_art.ascii import game_over_ascii, thanks
+from about.about import logan, anthony, nick, nebiyu
 import pytest
 from io import StringIO
 
-def test_menu(monkeypatch):
-    input_mock = StringIO('a')
-    monkeypatch.setattr('sys.stdin', input_mock)
+def test_menu(mocker):
+    mocker.patch('builtins.input', side_effect=['z','a'])
     assert menu() == 'a'
 
 def test_game_over(capsys):
@@ -24,3 +24,35 @@ def test_print_lives():
 
 def test_print_points():
     pass
+
+
+def test_print_about_menu(devs, capsys):
+    print_about_menu(devs)
+    captured = capsys.readouterr()
+    assert len(captured.out) == 93
+
+
+def test_run_app(mocker):
+    # '' Press enter at welcome
+    # 'r' Go to rules
+    # '' Go back
+    # '' Go to devs
+    # '1' Go t first dev
+    # 'x' Back dev input
+    # '0' Exit devs
+    #  'q' quit app
+    user_inputs = ['','r', '', 'd', '1', 'x', '0', 'q']
+
+    with pytest.raises(SystemExit):
+        mocker.patch('builtins.input', side_effect=user_inputs)
+        run_app()
+
+
+@pytest.fixture()
+def devs():
+    return {
+        '1': {'name': 'Anthony Beaver', 'method': anthony},
+        '2': {'name': 'Nick Dorkins', 'method': nick},
+        '3': {'name': 'Logan Jones', 'method': logan},
+        '4': {'name': 'Nebiuy Kifle', 'method': nebiyu}
+        }
